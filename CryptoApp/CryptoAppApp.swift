b9b7906b57aedc5267fa90bfa3f806c7cdd91186
co.abu <anizomxoja@gmail.com>
2022-09-14 13:32:05 +0500

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Abduljamil SwiftCoder on 14/09/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
